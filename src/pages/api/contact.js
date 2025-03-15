1086ca58463194a9271b332978f33f48a69cf4e9
Nikieta <user.email like : "nikieta09@gmail.com>
2025-03-15 22:31:59 +0530

    cors

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://nikieta.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
