8d7eadb3fffbaa6ffc18e2bb10d6edec99f31910
Nikieta <user.email like : "nikieta09@gmail.com>
2025-03-16 09:11:44 +0530

    few more

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -11,6 +11,8 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
+  
+
   if (req.method === "POST") {
     const { fullName, email, subject, message } = req.body;
 
